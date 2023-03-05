868c1473421963d5bf37539b30d3541321e7942e
Sergey_Puchnin <puchnin1996@mail.ru>
2023-03-05 14:21:59 +0300

    Examples004_Random

---
+++ Examples004_Random/Program.cs
@@ -0,0 +1,6 @@
+﻿int numderA = new Random().Next(1, 10); // 1 2 3 ... 9
+Console.WriteLine(numderA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numderA + numberB;
+Console.WriteLine(result);
